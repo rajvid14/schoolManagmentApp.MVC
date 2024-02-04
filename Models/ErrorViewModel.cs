736e156e5ff5d83bced6f5544c42e202d8517299
Rajvi <rajvigd14@gmail.com>
2024-02-04 23:56:52 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace schoolManagmentApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
